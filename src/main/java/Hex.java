5f0b658e4a2967ca6f479faf9d20d28160e170df
John Henning <jlhbaseball15@me.com>
2017-03-16 12:00:50 -0400

    fixed repo

---
+++ src/main/java/Hex.java
@@ -0,0 +1,35 @@
+/**
+ * Created by johnhenning on 3/15/17.
+ */
+public class Hex {
+    private int x;
+    private int y;
+    private TerrainType terrain;
+    private int tileIndex;
+    private int MeepleCount;
+    private boolean Totoro;
+
+    public Hex(int x, int y, TerrainType terrain) {
+        this.x = x;
+        this.y = y;
+        this.terrain = terrain;
+    }
+
+    public int getx() {
+        return x;
+    }
+
+    public int gety() {
+        return y;
+    }
+
+    public TerrainType getTerrain() {
+        return terrain;
+    }
+
+    public void setTileIndex(int tileIndex) {
+        this.tileIndex = tileIndex;
+    }
+
+}
+
