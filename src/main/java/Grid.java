0f7d09eba06abf46832c8a0f8b5de981ed802288
kyle <kbradley09@gmail.com>
2017-03-17 00:51:39 -0400

    reordered functions

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -32,6 +32,19 @@ public class Grid {
         return true;
     }
 
+    public boolean gridEmpty(){
+        return placedTiles.isEmpty();
+    }
+
+    public boolean CheckForUnoccupiedHexes(Tile tile){ //changed to public so I can use in tests
+        for (Hex hex : tile.getHexes()) {
+            if (gameboard[hex.getx()][hex.gety()] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean LevelTile(Tile tile) {
         return true;
     }
@@ -49,57 +62,13 @@ public class Grid {
         hex.setTileIndex(placedTiles.size() - 1);
     }
 
-    public boolean CheckForUnoccupiedHexes(Tile tile){ //changed to public so I can use in tests
-        for (Hex hex : tile.getHexes()) {
-            if (gameboard[hex.getx()][hex.gety()] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
+
 
     private boolean CheckForAdjacentHex(Tile tile){
-    //if y is odd, then x in top left and bottom left is equal of that y value
-        //then x in top right and bottom right is greater by one. y is minus one for top and greater by one on bottom
 
-        for (Hex hex : tile.getHexes()) {
-            if(hex.gety() % 2 != 0){
-                if (gameboard[hex.getx()][hex.gety() - 1] != null &&
-                        gameboard[hex.getx() + 1][hex.gety() - 1] != null) {
-                    //finish
-                    return true;
-                }
-                else if (gameboard[hex.getx()][hex.gety() + 1] != null &&
-                        gameboard[hex.getx() + 1][hex.gety() + 1] != null) {
-                    //finish
-                    return true;
-                }
-                else if (gameboard[hex.getx() + 1][hex.gety()] != null &&
-                        gameboard[hex.getx() + 1][hex.gety() - 1] != null ) {
-                    //finish
-                    return true;
-                }
-                else if (gameboard[hex.getx() + 1][hex.gety()] != null &&
-                        gameboard[hex.getx() + 1][hex.gety() + 1] != null ) {
-                    //finish
-                    return true;
-                }
-                else if (gameboard[hex.getx() - 1][hex.gety()] != null &&
-                        gameboard[hex.getx()][hex.gety() - 1] != null ) {
-                    //finish
-                    return true;
-                }
-                else if (gameboard[hex.getx() - 1][hex.gety()] != null &&
-                        gameboard[hex.getx()][hex.gety() + 1] != null ) {
-                    //finish
-                    return true;
-                }
-            }
-        }
+        //if the tile being placed
+
         return true;
     }
 
-    public boolean gridEmpty(){
-        return placedTiles.isEmpty();
-    }
 }
