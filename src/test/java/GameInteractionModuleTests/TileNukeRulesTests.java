13fa643605ae0ca2adaa92ef32b6c851858b8a33
git4Dan <daniel002@ufl.edu>
2017-04-04 20:55:17 -0400

    Function for tests

--- src/test/java/GameInteractionModuleTests/TileNukeRulesTests.java
+++ src/test/java/GameInteractionModuleTests/TileNukeRulesTests.java
@@ -278,6 +278,77 @@ public class TileNukeRulesTests{
     }
 
 
+    public  void setupHexAndTilesOnGameState(GameState game){
+        Hex[] hexes1 = new Hex[3];
+        Hex[] hexes2 = new Hex[3];
+        Hex[] hexes3 = new Hex[3];
+        Hex[] hexes4 = new Hex[3];
+        Hex[] hexes5 = new Hex[3];
+
+        Coordinate hexes1Coord1 = new Coordinate(101, 99);
+        Coordinate hexes1Coord2 = new Coordinate(101, 100);
+        Coordinate hexes1Coord3 = new Coordinate(102, 100);
+
+        Coordinate hexes2Coord1 = new Coordinate(102, 102);
+        Coordinate hexes2Coord2 = new Coordinate(101, 101);
+        Coordinate hexes2Coord3 = new Coordinate(101, 102);
+
+        Coordinate hexes3Coord1 = new Coordinate(103, 98);
+        Coordinate hexes3Coord2 = new Coordinate(102, 98);
+        Coordinate hexes3Coord3 = new Coordinate(102, 99);
+
+        Coordinate hexes4Coord1 = new Coordinate(98, 100);
+        Coordinate hexes4Coord2 = new Coordinate(99, 100);
+        Coordinate hexes4Coord3 = new Coordinate(98, 101);
+
+        Coordinate hexes5Coord1 = new Coordinate(99, 98);
+        Coordinate hexes5Coord2 = new Coordinate(98, 99);
+        Coordinate hexes5Coord3 = new Coordinate(98, 98);
+
+        hexes1[0] = new Hex(hexes1Coord1, TerrainType.VOLCANO);
+        hexes1[1] = new Hex(hexes1Coord2, TerrainType.LAKE);
+        hexes1[2] = new Hex(hexes1Coord3, TerrainType.GRASSLAND);
+
+        hexes2[0] = new Hex(hexes2Coord1, TerrainType.VOLCANO);
+        hexes2[1] = new Hex(hexes2Coord2, TerrainType.GRASSLAND);
+        hexes2[2] = new Hex(hexes2Coord3, TerrainType.GRASSLAND);
+
+        hexes3[0] = new Hex(hexes3Coord1, TerrainType.VOLCANO);
+        hexes3[1] = new Hex(hexes3Coord2, TerrainType.GRASSLAND);
+        hexes3[2] = new Hex(hexes3Coord3, TerrainType.GRASSLAND);
+
+        hexes4[0] = new Hex(hexes4Coord1, TerrainType.VOLCANO);
+        hexes4[1] = new Hex(hexes4Coord2, TerrainType.JUNGLE);
+        hexes4[2] = new Hex(hexes4Coord3, TerrainType.ROCKY);
+
+        hexes5[0] = new Hex(hexes5Coord1, TerrainType.VOLCANO);
+        hexes5[1] = new Hex(hexes5Coord2, TerrainType.JUNGLE);
+        hexes5[2] = new Hex(hexes5Coord3, TerrainType.JUNGLE);
+
+        Tile tile1 = new Tile(hexes1);
+        Tile tile2 = new Tile(hexes2);
+        Tile tile3 = new Tile(hexes3);
+        Tile tile4 = new Tile(hexes4);
+        Tile tile5 = new Tile(hexes5);
+
+
+        try {game.placeTile(tile1);}
+        catch (AssertionError e) { exceptionThrown = true; }
+        assert  !exceptionThrown;
+        try {game.placeTile(tile2);}
+        catch (AssertionError e) { exceptionThrown = true; }
+        assert  !exceptionThrown;
+        try {game.placeTile(tile3);}
+        catch (AssertionError e) { exceptionThrown = true; }
+        assert  !exceptionThrown;
+        try {game.placeTile(tile4);}
+        catch (AssertionError e) { exceptionThrown = true; }
+        assert  !exceptionThrown;
+        try {game.placeTile(tile5);}
+        catch (AssertionError e) { exceptionThrown = true; }
+        assert  !exceptionThrown;
+    }
+
     //TODO: test whether or not a settlement can be completly wiped out
     public  void setupTilesOnGameState(GameState game){
         try {game.placeTile(tile);}
