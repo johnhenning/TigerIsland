028b3b70d0178cc19c174674eaaf1e1bb4214741
git4Dan <daniel002@ufl.edu>
2017-04-11 22:29:35 -0400

    added more AI functions

--- src/main/java/IOModule/AI.java
+++ src/main/java/IOModule/AI.java
@@ -1,12 +1,14 @@
 package IOModule;
 
 import GameInteractionModule.Rules.BuildRules;
+import GameInteractionModule.Rules.SettlementFoundationRules;
 import GameInteractionModule.Rules.TilePlacementRules;
 import GameInteractionModule.Turn;
 import GameStateModule.*;
 import ServerModule.Adapter;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by johnhenning on 4/4/17.
@@ -78,13 +80,37 @@ public class AI implements Player {
 
     }
 
-    public ArrayList<Settlement> getPlayerSettlementsLessThanFive(GameState gameState, Player player){
+
+    public boolean isNewSettlementLarger(Settlement previous, Settlement current){
+        if(previous == null){
+            return true;
+        }
+
+        return previous.getSettlementCoordinates().size() < current.getSettlementCoordinates().size();
+
+    }
+
+    public Hex getBestHexForFoundation(GameState gameState){
+        ArrayList<Settlement> lessThanSizeFivePlayerSettlements = getPlayerSettlementsLessThanFive(gameState);
+        Settlement bestSettlment = null;
+        Hex bestHex = null;
+        for(Settlement s: lessThanSizeFivePlayerSettlements){
+            ArrayList<Hex> adjacentHexes = getHexesAdjacentToSettlementLessThanFive(s, gameState);
+            if((adjacentHexes != null) && isNewSettlementLarger(bestSettlment, s)) {
+                bestSettlment = s;
+                bestHex = adjacentHexes.get(0);
+            }
+        }
+        return bestHex;
+    }
+
+    public ArrayList<Settlement> getPlayerSettlementsLessThanFive(GameState gameState){
 
         ArrayList<Settlement> settlementList = gameState.getSettlementList();
         ArrayList<Settlement> playerSettlements = new ArrayList<>();
         ArrayList<Settlement> playerSettlementsLessThanFive = new ArrayList<>();
         for(Settlement s: settlementList){
-            if(s.getOwner().equals(player)){
+            if(s.getOwner().equals(gameState.getCurrentPlayer())){
                 playerSettlements.add(s);
             }
         }
@@ -97,18 +123,63 @@ public class AI implements Player {
         return playerSettlementsLessThanFive;
     }
 
-    public ArrayList<Hex> getHexesAdjacentToSettlementsLessThanFive(ArrayList<Settlement> playerSettlements, GameState gameState){
+    public ArrayList<Hex> getHexesAdjacentToSettlementLessThanFive(Settlement playerSettlement, GameState gameState){
 
         Grid gameboard = gameState.getGameboard();
         ArrayList<Hex> adjacentHexes = new ArrayList<>();
 
-        for(Settlement s: playerSettlements){
-            for(Coordinate c: s.getSettlementCoordinates()){
-                Hex hex = gameboard.getHexFromCoordinate(c);
-                adjacentHexes.addAll(TilePlacementRules.getAdjacentHexes(hex, gameboard));
+
+        for(Coordinate c: playerSettlement.getSettlementCoordinates()){
+            Hex hex = gameboard.getHexFromCoordinate(c);
+            for(Hex h : TilePlacementRules.getAdjacentHexes(hex, gameboard)){
+                if(!containsHex(adjacentHexes, h)){
+                    adjacentHexes.add(h);
+                }
             }
         }
 
+
+        for(int i = 0; i < adjacentHexes.size(); i++){
+            if(!SettlementFoundationRules.isValidFoundation(adjacentHexes.get(i), gameState.getCurrentPlayer())){
+                adjacentHexes.remove(i--);
+            }
+        }
+
+        return adjacentHexes;
+    }
+
+    public static boolean containsHex(ArrayList<Hex> hexes, Hex hex){
+        for(Hex h : hexes){
+            if(h.equals(hex)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public ArrayList<Hex> getHexesAdjacentToSettlementsLessThanFive(ArrayList<Settlement> playerSettlements, GameState gameState) {
+
+        Grid gameboard = gameState.getGameboard();
+        ArrayList<Hex> adjacentHexes = new ArrayList<>();
+
+        for (Settlement s : playerSettlements) {
+            for (Coordinate c : s.getSettlementCoordinates()) {
+                Hex hex = gameboard.getHexFromCoordinate(c);
+                for (Hex h : TilePlacementRules.getAdjacentHexes(hex, gameboard)) {
+                    if (!containsHex(adjacentHexes, h)) {
+                        adjacentHexes.add(h);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < adjacentHexes.size(); i++){
+            Hex hex = adjacentHexes.get(i);
+            if(!SettlementFoundationRules.isValidFoundation(hex, gameState.getCurrentPlayer())){
+                adjacentHexes.remove(i--);
+            }
+        }
         return adjacentHexes;
     }
 }
